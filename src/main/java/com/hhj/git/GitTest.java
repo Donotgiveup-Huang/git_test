370c2ecd45c40ec766e6b72dfceb94166182a3be
hhj <huang_1026@foxmail.com>
2022-07-13 21:05:02 +0800

    master commit

--- src/main/java/com/hhj/git/GitTest.java
+++ src/main/java/com/hhj/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("git test3");
         System.out.println("git test4");
         System.out.println("hot-fix test");
+        System.out.println("master1 test");
     }
 }
