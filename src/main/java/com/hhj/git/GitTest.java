8e7b6e019851278037bb1975a73c5b2e8f9872c6
hhj <huang_1026@foxmail.com>
2022-07-13 21:33:23 +0800

    push commit

--- src/main/java/com/hhj/git/GitTest.java
+++ src/main/java/com/hhj/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("master1 test");
         System.out.println("hot-fix test2");
+        System.out.println("push test！");
     }
 }
